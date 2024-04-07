14dd3689814f836970b62559f7a615b7b0f49045
Priyanthan <priyanthan2@gmail.com>
2024-04-07 13:15:39 +0530

    POST Data with flask py
    Defined posts that contains  list of blog posts and we call that in side home.html.
    
    using { %  for  post in posts %}
    <h1>{{post.title}}</h1> this is for title
    <p> By {{post.author}} on {{post.date_posted}}</p>
    {% endfor %} with end
    
    actually we create a for loop and getting its data from it

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
 
+posts = [
+    {
+        'author':'cory schafer',
+        'title':'Blog post',
+        'content':'First post content',
+        'date_posted':'April 20, 2010'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'April 21, 2010'
+    }
+    
+    
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
