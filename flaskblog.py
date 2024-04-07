bc92181d2970073771c5fe52de50979667db6a5e
Priyanthan <priyanthan2@gmail.com>
2024-04-07 17:39:37 +0530

    Creating Forms with flask
    Created Register form

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template,url_for
 
+from forms import RgistrationForm,LoginForm
+
+
+
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '3fa97c5c97086b4c7862043b86d685d9'
+
+
 posts = [
     {
         'author':'cory schafer',
@@ -17,7 +28,7 @@ posts = [
     
 ]
 
-app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/home")
@@ -28,6 +39,19 @@ def hello_world():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register")
+def register():
+    form = RgistrationForm()
+    return render_template('register.html', title='Register',form=form)
+    
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login',form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
